5285f64694fcc413a17bbcbd4c93d33525360d7b
Rohit Aakash <5477233+rohitaakash@users.noreply.github.com>
2017-12-04 04:13:45 -0500

    Added merge intervals

--- src/MergeIntervals.java
+++ src/MergeIntervals.java
@@ -1,59 +1,89 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
 public class MergeIntervals {
 	
-	public List<Interval> merge(List<Interval> intervals){
-        int[][] arr = new int[intervals.size()][2];
-        
-        int j = 0;
-        for(Interval i: intervals){
-            arr[j][0] = i.start;
-            arr[j][1] = i.end;
-        }
-        
-        Arrays.sort(arr);
-        
-        
-        
-        return null;
-    }
+	public static List<Interval> merge(List<Interval> intervals) {
+		if (intervals.isEmpty())
+			return intervals;
+
+		List<Interval> merged = new ArrayList<>();
+		Collections.sort(intervals, new StartComparator());
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		int current = intervals.get(0).start;
+		map.put(intervals.get(0).start, intervals.get(0).end);
+
+		for (int i = 1; i < intervals.size(); i++) {
+			if((map.containsKey(intervals.get(i).start) && map.get(intervals.get(i).start) <= intervals.get(i).end)
+					|| (map.get(current) >= intervals.get(i).start) && map.get(current) <= intervals.get(i).end) {
+				map.put(current, intervals.get(i).end);
+			}else if(map.get(current)< intervals.get(i).start) {
+				map.put(intervals.get(i).start, intervals.get(i).end);
+				current = intervals.get(i).start;
+			}
+		}
+
+		for (int i : map.keySet()) {
+			Interval k = new Interval(i, map.get(i));
+			merged.add(k);
+		}
+		return merged;
+
+	}
+
 	
 	public List<Interval> merge2(List<Interval> intervals) {
-        List<Interval> merged = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for(Interval i: intervals){
-            if(!map.containsKey(getStart(intervals, i.start))) map.put(getStart(intervals, i.start), getEnd(intervals, i.end));            
-        }
-        
-        for(int i: map.keySet()){
-            Interval j = new Interval(i, map.get(i));
-            merged.add(j);
-        }
-        return merged;
-    }
+		List<Interval> merged = new ArrayList<>();
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		for (Interval i : intervals) {
+			if (!map.containsKey(getStart(intervals, i.start)))
+				map.put(getStart(intervals, i.start), getEnd(intervals, i.end));
+		}
+
+		for (int i : map.keySet()) {
+			Interval j = new Interval(i, map.get(i));
+			merged.add(j);
+		}
+		return merged;
+	}
 
 	public int getStart(List<Interval> intervals, int start) {
 		int s = start;
-		for(Interval i: intervals){
-            if(i.start < start && i.end >= start){
-                s = getStart(intervals, i.start);
-            }
-        }
+		for (Interval i : intervals) {
+			if (i.start < start && i.end >= start) {
+				s = getStart(intervals, i.start);
+			}
+		}
 		return s;
 	}
 
 	public int getEnd(List<Interval> intervals, int end) {
 		int e = end;
-		for(Interval i: intervals){
-            if(i.start <= end && i.end > end){
-                e = getEnd(intervals, i.end);
-            }
-        }
+		for (Interval i : intervals) {
+			if (i.start <= end && i.end > end) {
+				e = getEnd(intervals, i.end);
+			}
+		}
 		return e;
+	}
+
+	public static void main(String[] args) {
+		List<Interval> intervals = new ArrayList<>();
+		intervals.add(new Interval(1, 4));
+		intervals.add(new Interval(0, 4));
+		// intervals.add(new Interval(8,10));
+		// intervals.add(new Interval(15,18));
+
+		List<Interval> result = merge(intervals);
+
+		for (Interval i : result) {
+			System.out.println(i.start + "," + i.end);
+		}
 	}
 }
 
@@ -70,4 +100,18 @@ class Interval {
 		start = s;
 		end = e;
 	}
+}
+
+class StartComparator implements Comparator<Interval> {
+
+	@Override
+	public int compare(Interval o1, Interval o2) {
+		// TODO Auto-generated method stub
+		if (o1.start < o2.start)
+			return -1;
+		if (o1.start > o2.start)
+			return 1;
+		return 0;
+	}
+
 }
