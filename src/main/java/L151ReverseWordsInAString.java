c66fb4baeb5def382e687201e619ac017bc45b49
Rohit Aakash <rohit.aakash13@gmail.com>
2018-11-22 20:06:07 -0500

    Added code to reverse words in a string

--- src/main/java/L151ReverseWordsInAString.java
+++ src/main/java/L151ReverseWordsInAString.java
@@ -12,17 +12,14 @@ public class L151ReverseWordsInAString {
 		int i = s.length()-1, k;
 		
 		while(i>=0 && s.charAt(i) == ' ') i--;
-		System.out.println(i);
+
 		for(int j=i; j>=0; j--) {
-			System.out.println("j= " + j);
 			for(k=j; k>=0 && s.charAt(k)!=' '; k--);
-			System.out.println("k= " + k);
 			if(k != j) {
-				sb.append(s.substring(k+1, j+1));
+				sb.append(s, k+1, j+1);
 				sb.append(' ');
 				j=k+1;
 			}
-			
 		}
 		
 		if(sb.toString().equals("")) return "";
