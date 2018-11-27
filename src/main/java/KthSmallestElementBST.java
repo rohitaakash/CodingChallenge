6f5ffd2f5ee98d2e148ee33b42fc2f1169e590ee
Rohit Aakash <rohit.aakash13@gmail.com>
2018-11-26 22:01:47 -0500

    Minor changes to find kth smallest element in a binary search tree

--- src/main/java/KthSmallestElementBST.java
+++ src/main/java/KthSmallestElementBST.java
@@ -2,45 +2,35 @@ import java.util.ArrayList;
 
 public class KthSmallestElementBST {
 
-	public int kthSmallest(TreeNode1 root, int k) {
-		
-		ArrayList<Integer> list = new ArrayList<>();
-		inorderTraversal(root, list);
-		return list.get(k-1);
+    public static void main(String[] args) {
 
-	}
-	
-	public void inorderTraversal(TreeNode1 root, ArrayList<Integer> list) {
-		if(root == null) return;
-		
-		if(root.left != null) inorderTraversal(root.left, list);
-		list.add(root.val);
-		if(root.right != null) inorderTraversal(root.right, list);
-		
-	}
-	
-	public static void main(String[] args) {
-		
-		TreeNode1 root = new TreeNode1(15);
-		root.left = new TreeNode1(4);
-		root.right = new TreeNode1(17);
-		root.left.left = new TreeNode1(3);
-		root.left.right = new TreeNode1(8);
-		root.left.right.right = new TreeNode1(9);
-		
-		KthSmallestElementBST kthSmallestElementBST = new KthSmallestElementBST();
-		System.out.println(kthSmallestElementBST.kthSmallest(root, 3));
-		
-	}
+        TreeNode root = new TreeNode(15);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(17);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(8);
+        root.left.right.right = new TreeNode(9);
 
-}
+        KthSmallestElementBST kthSmallestElementBST = new KthSmallestElementBST();
+        System.out.println(kthSmallestElementBST.kthSmallest(root, 3));
 
-class TreeNode1 {
-	int val;
-	TreeNode1 left;
-	TreeNode1 right;
+    }
 
-	TreeNode1(int x) {
-		val = x;
-	}
+    private int kthSmallest(TreeNode root, int k) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        inorderTraversal(root, list);
+        return list.get(k - 1);
+
+    }
+
+    private void inorderTraversal(TreeNode root, ArrayList<Integer> list) {
+        if (root == null) return;
+
+        if (root.left != null) inorderTraversal(root.left, list);
+        list.add(root.val);
+        if (root.right != null) inorderTraversal(root.right, list);
+
+    }
+
 }
